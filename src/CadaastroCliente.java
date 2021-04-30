98ca58dc875d6c93f74f7fe442294fd3b17b69ff
Alejandro Rendon <@rendonalejandro.713@gmail.com>
2021-04-30 15:02:54 -0300

    first commit

---
+++ src/CadaastroCliente.java
@@ -0,0 +1,40 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CadaastroCliente {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+	
+ String nome;
+ String cpf;
+ int idade;
+ double altura;
+ 
+ System.out.println("Informe seu nome:");
+ nome = sc.nextLine();
+ System.out.println("Informe seu CPF:");
+ cpf = sc.nextLine();
+ System.out.println("Informe sua Idade:");
+ idade = sc.nextInt();
+ System.out.println("Qual sua altura");
+ altura = sc.nextDouble();
+System.out.println();
+System.out.println();
+System.out.println ("Nome: " + nome);
+System.out.println( "cpf: " + cpf);
+System.out.println("idade:" + idade);
+System.out.println("altura" + altura); 
+System.out.println("Seja ben-vindo!" + nome);
+
+sc.close();
+
+
+ 
+	
+
+	}
+
+}
