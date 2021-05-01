31c18d8caa1824934294b74fb89fb5eaf9770f24
Alejandro Rendon <@rendonalejandro.713@gmail.com>
2021-05-01 16:43:50 -0300

    Cadastro de clientre

--- src/CadaastroCliente.java
+++ src/CadaastroCliente.java
@@ -10,20 +10,25 @@ public class CadaastroCliente {
 	
  String nome;
  String cpf;
+ String sexo;
  int idade;
  double altura;
  
+ 
  System.out.println("Informe seu nome:");
  nome = sc.nextLine();
- System.out.println("Informe seu CPF:");
+ System.out.println("Infome a inicial do seu sexo (M) (F) : ");
+ sexo = sc.nextLine();
+ System.out.println("Informe seu CPF: ");
  cpf = sc.nextLine();
  System.out.println("Informe sua Idade:");
  idade = sc.nextInt();
- System.out.println("Qual sua altura");
+ System.out.println("Qual sua altura ");
  altura = sc.nextDouble();
 System.out.println();
 System.out.println();
 System.out.println ("Nome: " + nome);
+System.out.println ("sexo : " + sexo);
 System.out.println( "cpf: " + cpf);
 System.out.println("idade:" + idade);
 System.out.println("altura" + altura); 
